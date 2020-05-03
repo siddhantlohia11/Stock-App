30af2b1d434a3512213c6e578540987abd300b4a
shivay16 <shivamy1593@gmail.com>
2020-05-03 18:21:44 +0530

    Newly Designed Navbar

---
+++ public/js/navbar.js
@@ -0,0 +1,30 @@
+var hamburger = document.getElementById("hamburger");
+var drop = document.getElementById("drop");
+var x = document.getElementsByClassName("toNext"); 
+var close = document.getElementById("close");
+var i;
+
+hamburger.addEventListener("click", function(){
+	setTimeout(function(){
+		for (i = 0; i < x.length; i++) {
+			x[i].style.fontSize = "50px";
+			x[i].style.display = "block";
+		}
+		close.style.display = "block";
+	}, 200);
+	drop.style.zIndex = "100000";
+	drop.style.height = "100vh";
+	drop.style.transition = "all 0.5s";
+});
+
+close.addEventListener("click", function(){
+	drop.style.height = "0";
+	drop.style.transition = "all 0.5s";
+	setTimeout(function(){
+	for (i = 0; i < x.length; i++) {
+		x[i].style.display = "none";
+		x[i].style.display = "none";
+	}
+	close.style.display = "none";
+	},100);
+})
