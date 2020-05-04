2e1f77641a02d48827d4ce3c4984dd3575f380eb
siddhantlohia11 <sidhantlohia11@gmail.com>
2020-05-04 16:50:42 +0530

    Final with gitignore

--- app.js
+++ app.js
@@ -279,7 +279,10 @@ function escapeRegex(text) {
     return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
 };
 
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function() {
+  console.log("Server has started Successfully");
 });
