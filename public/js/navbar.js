c28722544409d66eb17354122c783448457fcd55
shivay16 <shivamy1593@gmail.com>
2020-05-04 12:45:21 +0530

    working fine on small screen

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -2,6 +2,7 @@ var hamburger = document.getElementById("hamburger");
 var drop = document.getElementById("drop");
 var x = document.getElementsByClassName("toNext"); 
 var close = document.getElementById("close");
+var y = document.getElementsByClassName("next");
 var i;
 
 hamburger.addEventListener("click", function(){
@@ -28,4 +29,30 @@ close.addEventListener("click", function(){
 	close.style.display = "none";
 	},100);
 })
+
+hamburger.addEventListener("click", function(){
+	setTimeout(function(){
+		for (i = 0; i < x.length; i++) {
+			y[i].style.fontSize = "20px";
+			y[i].style.display = "block";
+		}
+		close.style.display = "block";
+	}, 200);
+	drop.style.zIndex = "100000";
+	drop.style.height = "100vh";
+	drop.style.transition = "all 0.5s";
+});
+
+close.addEventListener("click", function(){
+	drop.style.height = "0";
+	drop.style.transition = "all 0.5s";
+	setTimeout(function(){
+	for (i = 0; i < x.length; i++) {
+		y[i].style.display = "none";
+		y[i].style.display = "none";
+	}
+	close.style.display = "none";
+	},100);
+})
+
 
